3793ed687e42c262555df136853b845df9a862f5
SauravpalSinghRana <sauravpal1718.be20@chitkara.edu.in>
2022-08-04 14:24:22 +0530

    Update 04August2022.js

--- 04August2022.js
+++ 04August2022.js
@@ -17,28 +17,3 @@ count = count+1;
 
     //document.getElementById("p1").innerHTML = l+r+k;
 }
-console.log(new Date().getDay());
-let day;
-switch (new Date().getDay()){
-    case 0:
-    day = "Sunday";
-    break;
-    case 1:
-        day = "Monday";
-        break;
-        case 2:
-            day = "Tuesday";
-            break;
-            case 3:
-                day = "Wednesday";
-                break;
-                case 4:
-                    day = "Thursday";
-                    break;
-                    case 5:
-                        day = "Friday";
-                        break;
-                        case 6:
-                            day = "Saturday";
-                            break;
-}
