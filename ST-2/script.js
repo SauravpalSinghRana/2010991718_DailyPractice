41c7a2df58e480c100920e7e056f790d4b7f7bae
SauravpalSinghRana <sauravpal1718.be20@chitkara.edu.in>
2022-08-09 09:26:04 +0530

    08/August/2022

---
+++ ST-2/script.js
@@ -0,0 +1,20 @@
+function myFunction() {
+  var x,
+    borderRadius,
+    sum = 0;
+  let a = document.getElementById("a").value;
+  let aCount=a.toString().length;
+  if (aCount== 5) {
+    while (a) {
+      x = a % 10;
+      sum += x*5;
+      a = Math.floor(a / 10);
+    }
+    borderRadius = Math.floor(sum / 5);
+    document.getElementById("create").style.width = sum + "px";
+    document.getElementById("create").style.height = sum + "px";
+    document.getElementById("create").style.borderRadius = borderRadius + "px";
+  } else {
+    alert("Please enter a 5-digit number");
+  }
+}
